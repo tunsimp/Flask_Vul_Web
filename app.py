e75222f4a3d9698b36d164c82fc26df408669c93
tunsimp <nguyenquoctuanwar@gmail.com>
2024-11-29 22:38:21 +0700

    Add logout, Navbar

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask,render_template,request,session
-
+from flask import Flask,render_template,request,session,redirect,url_for
+from datetime import timedelta
 app= Flask(__name__)
+app.debug = True
 app.secret_key="2208"
+app.permanent_session_lifetime=timedelta(minutes=5)
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    if "user" in session:
+        user=session['user']
+        return render_template("index.html",username=user)
+    else: 
+        return redirect(url_for('login'))
 
 @app.route("/login", methods=["POST","GET"])
 def login():
@@ -12,13 +19,13 @@ def login():
         username=request.form['name']
         password=request.form['password']
         if username==password:
-            return render_template("index.html")
+            session['user']=username
+            return redirect(url_for('home'))
         else: 
             return render_template("login.html")
-    elif request.method=="GET":
+    else:
         return render_template("login.html")
-    else: 
-        return "Invalid Method"
+
 
 @app.route("/register", methods=["POST","GET"])
 def register():
@@ -26,7 +33,7 @@ def register():
         username=request.form['name']
         password=request.form['password']
         repassword=request.form['repassword']
-        if username==password:
+        if username or password==repassword:
             return render_template("login.html")
         else: 
             return render_template("register.html")
@@ -34,6 +41,11 @@ def register():
         return render_template("register.html")
     else: 
         return "Invalid Method"
+
+@app.route("/logout")
+def logout():
+    session.pop("user",None)
+    return redirect(url_for('login'))
 
 if __name__=="__main__":
     app.run()
