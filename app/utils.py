70d471236ae8d26a32ffb88eeb1a1673c83816aa
tunsimp <nguyenquoctuanwar@gmail.com>
2024-12-20 16:35:46 +0700

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -3,7 +3,5 @@
 import re
 
 def is_valid_input(input_str):
-    """
-    Validate input to allow only alphanumeric characters and underscores.
-    """
-    return re.match("^[a-zA-Z0-9_]+$", input_str) is not None
+
+    return re.match("^[a-zA-Z0-9_\'\- ]+$", input_str) is not None
